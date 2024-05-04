1772e3665a187b646511df1f782affcd75823a7f
afriza <bagusrahmat582@gmail.com>
2024-05-04 23:50:19 +0700

    add:router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,38 @@
 import React from 'react'
-import './layout.scss'
-import Navbar from './components/navbar/Navbar'
 import HomePage from './routes/homePage/HomePage'
+import ListPage from './routes/listPage/ListPage'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
+import Layout from './routes/layout/Layout'
+import SinglePage from './routes/singlePage/SinglePage'
 
 const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout/>,
+      children:[
+        {
+          path:"/",
+          element: <HomePage/>
+        },
+        {
+          path:"/list",
+          element:<ListPage/>
+        },
+        {
+          path:"/:id",
+          element:<SinglePage/>
+        }
+      ]
+    },
+  ]);
+
+
   return (
-    <div className="layout">
-      <div className="navbar">
-      <Navbar/>
-      </div>
-      <div className="content">
-        <HomePage/>
-      </div>
-    </div>
+    <RouterProvider router={router}/>
   )
 }
 
--- client/src/routes/homePage/HomePage.jsx
+++ client/src/routes/homePage/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./homePage.scss"
+import SearchBar from '../../components/searchBar/SearchBar' 
 
 const HomePage = () => {
   return (
@@ -8,6 +9,21 @@ const HomePage = () => {
             <div className="wrapper">
                 <h1 className='title'>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis rem, corporis officiis dolorem minus vel modi ex culpa vero reiciendis qui repudiandae! Dolorem consequuntur enim velit neque nobis fuga natus?</p>
+            <SearchBar/>
+            <div className="boxes">
+              <div className="box">
+                <h1>16+</h1>
+                <h2>Years of experience</h2>
+              </div>
+              <div className="box">
+                <h1>200</h1>
+                <h2>Award gained</h2>
+              </div>
+              <div className="box">
+                <h1>2000+</h1>
+                <h2>Property ready</h2>
+              </div>
+            </div>
             </div>
         </div>
         <div className="imgContainer">
